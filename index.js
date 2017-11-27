be600e4c81d396629bb30d6bf8c2d83398937568
taha vajed <taha.vajed@gmail.com>
2017-11-27 18:11:54 +0330

    little mistake fixed: use || istead of | for 'const port'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
-const port = process.env.PORT | 3300
+const port = process.env.PORT || 3000
 
 app = express();
 app.set('view engine', 'hbs')
